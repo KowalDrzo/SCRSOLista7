6df38d28447fd6549fa2a3153f2980a229bb0a2a
KowalDrzo <61516230+KowalDrzo@users.noreply.github.com>
2021-01-07 15:55:26 +0100

    Update Zad7.c

--- Zad7.c
+++ Zad7.c
@@ -1,20 +1,38 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+#define LWATKOW 8
+#define PROBY 600
 
 void *wypisz(void *idWatku) {
 
     int id;
     id = (long)idWatku;
-    printf("Hello SCR! Napisane przez wątek nr %d\n", id);
+
+    double x, y;
+    int licznik = 0;    
+
+    for(int i=0; i < PROBY; i++) {
+
+        x = drand48();
+        y = drand48();
+
+        if(sqrt(x*x+y*y) < 0.5) licznik++;
+    }
+
+    double pi = (4.0*licznik)/PROBY;
+
+    printf("Wątek %d uznaje, że liczba pi to: %f\n", id, pi);
     pthread_exit(NULL);
 }
 
 int main() {
 
-    pthread_t watek[3]; // Utworzenie tablicy wątków
+    pthread_t watek[LWATKOW]; // Utworzenie tablicy wątków
 
-    for(int i = 0; i < 3; i++)
+    for(int i = 0; i < LWATKOW; i++)
         if(pthread_create(&watek[i], NULL, wypisz, (void *)(long)i))
             printf("\nBłąd tworzenia wątku!\n");
 
